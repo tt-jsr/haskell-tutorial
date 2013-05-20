c515123054d53c593fb57edf634133f33e63b666
jeff <jeff.richards@tradingtechnologies.com>
2013-05-19 20:35:00 -0500

    Some file stuff

--- hello.hs
+++ hello.hs
@@ -1,9 +1,27 @@
+import System.IO
+import Data.Char(toUpper)
 
 main = do   
-    do putStrLn "Enter name: "
+    putStrLn "Enter name: "
     line <- getLine  
     let t = checkname line
     putStrLn t
+
+    inh <- openFile "input.txt" ReadMode
+    outh <- openFile "output.txt" WriteMode
+    mainloop inh outh
+    hClose inh
+    hClose outh
+
+mainloop :: Handle -> Handle -> IO ()
+mainloop inh outh = 
+    do ineof <- hIsEOF inh
+       if ineof
+       then     
+            return ()
+       else do inpStr <- hGetLine inh
+               hPutStrLn outh (map toUpper inpStr)
+               mainloop inh outh
 
 checkname :: String -> String
 checkname s 
