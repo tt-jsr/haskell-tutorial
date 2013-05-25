049a3abd724d885fb2ce26ea5c4e1f7e39c91cab
jeff <jeff.richards@tradingtechnologies.com>
2013-05-24 23:15:45 -0500

    Slight changes

--- file.hs
+++ file.hs
@@ -2,14 +2,14 @@
 import System.IO
 import System.Directory
 import Data.List
+import Data.Char
 
 main = do   
     files <- getDirectoryContents "."
-    showFiles (sort files)
+    showFiles (map (map toUpper) (sort files))
 
 showFiles :: [FilePath] -> IO ()
 showFiles (f:fs) = 
     do putStrLn f
        showFiles fs
 
-  
