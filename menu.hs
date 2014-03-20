464b41a99b3ee7f8bfe50df2c99661fc38f85264
Jeff Richards <jeff@debian-vm.homeland.net>
2014-03-19 21:10:41 -0500

    menu.hs

---
+++ menu.hs
@@ -0,0 +1,16 @@
+import System.IO
+import System.Directory
+import Data.List
+import Data.Char
+
+main =
+    do x <- getX
+       putStrLn x
+
+getX =
+    do return "My Shangri-La"
+       return "beneath"
+       return "the summer moon"
+       return "I will"
+       return "return"
+       return "again"
