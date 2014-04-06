ea23e97e60af872eff808be316ee2e3130f5b2ff
Jeff Richards <jeff@jeff-vm.homeland>
2014-04-06 14:04:15 -0500

    Defined 'Quad' data type This works similar to 'Either'

--- test.hs
+++ test.hs
@@ -1,38 +1,41 @@
 import System.Environment
 
 data Foo = Foo {
-    foo_x :: Integer,
-    foo_y :: Integer,
+    foo_x :: Integer, 
+    foo_y :: Integer, 
     foo_name :: String
 }
 
 data Man = Man {
-    man_height :: Integer,
-    man_weight :: Double,
+    man_height :: Integer, 
+    man_weight :: Integer, 
     man_name :: String
 }
 
 data Choo = Choo {
-    choo_arg1 :: Int,
-    choo_arg2 :: Int
+    choo_arg1 :: Integer, 
+    choo_arg2 :: Integer
 }
+
+
+data Quad a b c d = First a | Second b | Third c | Fourth d
 
 main = do   
     args <- getArgs
-    let x = create (head args)
+    let x = create (if length args == 0 then "No argument" else head args)
     putStrLn $ get x
 
-create :: String -> Either Foo Man
---create s = if s == "man" 
-            --then Right $ Man 60 180 "Jeff"
-            --else Left $ Foo 1 1 "position"
+create :: String -> Quad Foo Man Choo String
 create s
-    |s=="man" = Right $ Man 60 180 "Jeff"
-    |s=="foo" = Left $ Foo 1 1 "position"
-    |otherwise = Left $ Foo 1 1 "otherwise"
+    |s=="foo" = First (Foo 1 1 "position")
+    |s=="man" = Second (Man 60 180 "Jeff")
+    |s=="choo" = Third (Choo 10 20)
+    |otherwise = Fourth s
 
-get :: Either Foo Man -> String
-get s = case s of
-    (Left foo) -> foo_name foo
-    (Right man) -> man_name man
+get :: Quad Foo Man Choo String  -> String
+get e = case e of
+    First first -> "Have a foo: " ++ (foo_name first)
+    Second sec -> "Have a man: " ++ ( man_name sec)
+    Third third -> "Have a choo: " ++ show (choo_arg1 third) ++ "," ++ show (choo_arg2 third)
+    Fourth fourth -> "Error: " ++ fourth
 
